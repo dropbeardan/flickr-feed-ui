cab3a83c0a0886c7463eaa612eb3448bc3862bf9
dropbeardan <dan.ess.001@gmail.com>
2019-04-17 18:19:10 +1000

    Add Webpack and Typescript boilerplate

---
+++ src/app/index.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+export interface HelloProps {
+	compiler: string;
+	framework: string;
+}
+
+export class Hello extends React.Component<HelloProps, {}> {
+	render() {
+		return (
+			<React.Fragment>
+				<h1>
+					Hello from {this.props.compiler} and {this.props.framework}!
+				</h1>
+
+				<h1>Current Running Env Mode: {process.env.NODE_ENV}</h1>
+			</React.Fragment>
+		);
+	}
+}
+
+ReactDOM.render(
+	<Hello compiler="TypeScript" framework="React" />,
+	document.getElementById('REACT_APP')
+);
