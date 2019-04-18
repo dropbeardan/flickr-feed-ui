68c3fd02413cbbf4867ebb8c6503fbbf44eb1ab8
dropbeardan <dan.ess.001@gmail.com>
2019-04-18 21:42:56 +1000

    Add storybook addon to enable JSS theme for stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,11 @@
-import { configure } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
+
+import { withThemesProvider } from 'storybook-addon-jss-theme';
+
+import { theme } from '../src/app/Providers/JSSThemeProvider';
+
+const themes = [{ name: 'JSSProviderTheme', variables: theme }];
+addDecorator(withThemesProvider(themes));
 
 // Loads all files ending with ".stories.ts" or ".stories.tsx" in 'src' directory.
 const req = require.context('../src', true, /[.]stories[.](ts|js)x?$/);
